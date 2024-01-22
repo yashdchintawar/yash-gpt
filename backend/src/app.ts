3338259154bb9040422b11701a65d0ac35eddf21
Yash Chintawar <58319772+yashdchintawar@users.noreply.github.com>
2024-01-22 08:03:17 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,42 @@
+// import express from "express";
+// import { config } from "dotenv";
+// import morgan from "morgan";
+// import appRouter from "./routes/index.js";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// config();
+// const app = express();
+
+// //START -> middlewares
+// app.use(cors({ origin: "yash-gpt-backend.onrender.com", credentials: true }));
+// app.use(express.json());
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// //END -> middlewares
+
+// // START -> remove it in production
+// app.use(morgan("dev"));
+// // END -> remove it in production
+
+// app.use("/api/v1", appRouter);
+
+// export default app;
+
+
 import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 config();
 const app = express();
 
-//START -> middlewares
-app.use(cors({ origin: "yash-gpt-backend.onrender.com", credentials: true }));
+// START -> middlewares
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-//END -> middlewares
+// END -> middlewares
 
 // START -> remove it in production
 app.use(morgan("dev"));
